616c9989fd20b8e653a11be413433b87966dee22
Hamid Nur <hamidsharif2001@gmail.com>
2022-01-17 19:18:00 -0500

    fixes bug where counter will not reset

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,21 @@ $(document).ready(function() {
   console.log("hello")
   $("#tweet-text").on("input", function() {
     console.log(this.value); //The this keyword is a reference to the button
-    const variablesRemaining = 140 - this.value.length;
+    let variablesRemaining = 140 - this.value.length;
     console.log(variablesRemaining)
     $(".counter").text(variablesRemaining);
+    // if (variablesRemaining < 0) {
+    //   $(".counter").css("color","red");
+    // }
     if (variablesRemaining < 0) {
       $(".counter").css("color","red");
+    } else {
+      $(".counter").css("color","black");
     }
+  });
+  $( "#compose" ).submit(function( event ) {
+    event.preventDefault();
+    $(".counter").text(140);
   });
 });
 
